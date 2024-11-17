41a48accbaa867e28897808792cb2bfe4fd75aee
sherrera-prog <sergiocull@gmail.com>
2024-11-17 12:50:29 +0100

    Modificacion 'ganar.php' para incluir la lógica de funcionamiento

---
+++ ganar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ganador</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<h1>¡Felicidades!</h1>
+    <p>¡Enhorabuena! Has ganado :)</p>
+    <a href="index.php"><button>Jugar de nuevo</button></a>
+</body>
+</html>
