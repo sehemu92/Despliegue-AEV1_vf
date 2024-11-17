ef4b43109c508aaa1c10594cd06e100e5ae8a74a
sherrera-prog <sergiocull@gmail.com>
2024-11-17 13:08:08 +0100

    Nuevas palabras añadidas al juego en el archivo 'index.php'

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance', 'tiburon', 'gorila', 'leon'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
