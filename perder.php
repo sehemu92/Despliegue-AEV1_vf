35f4a4a5ca3ea316d50dcffb1528b6ce670cb6d5
sherrera-prog <sergiocull@gmail.com>
2024-11-17 13:02:43 +0100

    Modificación 'perder.php' para incluir la lógica de funcionamiento

---
+++ perder.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perdedor</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<h1>¡Lo siento!</h1>
+    <p>Has perdido :( La palabra era: <?php echo $_GET['palabra']; ?></p>
+    <a href="index.php"><button>Jugar de nuevo</button></a>
+    
+</body>
+</html>
